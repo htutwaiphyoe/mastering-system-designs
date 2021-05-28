b2ba2c38c21790c2c306c41390e563125c0c2701
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-05-29 00:48:32 +0630

    delete: entire project

--- src/api/firebase.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://blackdays-122021-default-rtdb.firebaseio.com/",
-});
--- src/components/UI/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import "./Loader.scss";
-
-const Loader = (props) => (
-    <section className="Loader">
-        <div className="loader"></div>
-    </section>
-);
-
-export default Loader;
